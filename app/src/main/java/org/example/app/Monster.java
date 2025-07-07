b7e4c11b09e9f0fe49c681d62c90d6f0553b9ccd
MatsuiYuto <m1m02530155@oit.ac.jp>
2025-07-07 10:58:47 +0900

    add Monster

---
+++ app/src/main/java/org/example/app/Monster.java
@@ -0,0 +1,22 @@
+package org.example.app;
+
+public class Monster {
+  private String name;
+  private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
+
+  Monster(int nameNum, int rareNum) {
+    this.name = this.summonMonster(nameNum);
+    this.rare = rareNum;
+  }
+
+  String summonMonster(int mnumber) {
+    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  @Override
+  public String toString() {
+
+    return this.name + ":レア度[" + this.rare + "]\n";
+  }
+}
