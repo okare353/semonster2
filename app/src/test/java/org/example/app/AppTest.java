b7b97fecef8ddbf9036a305b3533fa8c25851c7e
okare <e1c22010@oit.ac.jp>
2025-07-07 14:08:25 +0900

    set branch okada

--- app/src/test/java/org/example/app/AppTest.java
+++ app/src/test/java/org/example/app/AppTest.java
@@ -10,6 +10,6 @@ public class AppTest {
   @Test
   public void testAppHasAGreeting() {
     App classUnderTest = new App();
-    assertEquals("こんにちは SEMonster", classUnderTest.getGreeting());
+    assertEquals("こんにちは SEMonster ", classUnderTest.getGreeting());
   }
 }
