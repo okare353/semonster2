89b8de34f64163a41a67f1fd9a7061dc8a270957
okare <e1c22010@oit.ac.jp>
2025-07-07 10:29:01 +0900

    add App AppTest re

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -5,7 +5,7 @@ package org.example.app;
 
 public class App {
   public String getGreeting() {
-    return "こんにちは SEMonster";
+    return "Hello SEMonster";
   }
 
   public static void main(String[] args) {
